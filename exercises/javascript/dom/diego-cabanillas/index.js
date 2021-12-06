3c626569cff3a7590eed1f76cde6fb79b645640a
dgcabanillas <dg.cabanillas@uni.pe>
2021-12-06 18:44:07 -0500

    fix/fixing some bugs

--- exercises/javascript/dom/diego-cabanillas/index.js
+++ exercises/javascript/dom/diego-cabanillas/index.js
@@ -2,19 +2,22 @@ const input = document.getElementById('todoInput');
 const button = document.getElementById('addToDo');
 const list = document.querySelector('#container > ul');
 
-const createToDo = () => {
-  const todo = input.value;
-  return `
-    <li>
-      <span><i class="fa fa-trash"></i></span>' 
-      ${ todo } 
-    </li>
-  `
+const createToDoNode = ( todo ) => {
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const i = document.createElement('i');
+  i.classList.add('fa','fa-trash');
+  const textTodo = document.createTextNode( ' ' + todo );
+  span.appendChild(i);
+  li.appendChild(span);
+  li.appendChild(textTodo);
+  return li;
 }
 
 const addTask = () => {
-  if( input.value ) {
-    list.innerHTML = createToDo() + list.innerHTML;
+  const todo = input.value;
+  if( todo ) {
+    list.prepend( createToDoNode( todo ) );
     input.value = '';
   }
 }
@@ -24,5 +27,7 @@ button.addEventListener('click', () => { addTask() })
 list.addEventListener('click', (e) => {
   if( e.target.classList.contains('fa-trash') ) {
     e.target.parentNode.parentNode.remove();
+  } else if( e.target.tagName.toLowerCase().trim() === 'span' ) {
+    e.target.parentNode.remove();
   }
 })
