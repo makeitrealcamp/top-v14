0521c4d805f3ca8e6daf66630211bc93293c587b
Shymmy Garcia <shymmygarcia@MacBook-Pro.local>
2022-02-25 19:12:13 -0500

    add error to conecction db

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 
 export const createDbConnection = (dbURL: string) => {
-  mongoose.connect(dbURL)
+  mongoose
+    .connect(dbURL)
     .then(() => console.log('connected'))
-    .catch((err) => console.log('error on connection'));
+    .catch((err) => console.log(err));
   mongoose.connection.on('error', () => console.log('Error on db connection'));
   mongoose.connection.once('connected', () => console.log('db connected'));
-}
+};
